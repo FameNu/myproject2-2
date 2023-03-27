336713c36fe4302569224a92192268ebf17a017c
FameNu <phuwamet.fame@gmail.com>
2023-03-27 14:56:29 +0700

    New Method

--- src/main/java/project2Class2/MyProjectInClass.java
+++ src/main/java/project2Class2/MyProjectInClass.java
@@ -3,5 +3,10 @@ package project2Class2;
 public class MyProjectInClass {
     public static void main(String[] args) {
         System.out.println("Hello My world");
+        System.out.println(add(3));
+    }
+    
+    public static int add(int x){
+        return x + 3;
     }
 }
