5c802b670bba67c3c22a1b3200878b1832a5d375
FameNu <phuwamet.fame@gmail.com>
2023-03-27 17:33:48 +0700

    YOYO

--- src/main/java/project2Class2/MyProjectInClass.java
+++ src/main/java/project2Class2/MyProjectInClass.java
@@ -2,18 +2,30 @@ package project2Class2;
 
 public class MyProjectInClass {
     public static void main(String[] args) {
+//        try {
+//            int[] list = new int[10];
+//            System.out.println("list[10] is " + list[10]);
+//        }
+//        catch (ArithmeticException ex){
+//            System.out.println("ArithmeticException");
+//        }
+//        catch (RuntimeException ex){
+//            System.out.println("RuntimeException");
+//        }
+//        catch (Exception ex){
+//            System.out.println("Exception");
+//        }
         try {
-            int[] list = new int[10];
-            System.out.println("list[10] is " + list[10]);
+            System.out.println("Statement 1");
+            System.out.println(1/0);
+            System.out.println("Statement 2");
         }
-        catch (ArithmeticException ex){
-            System.out.println("ArithmeticException");
+        catch(IndexOutOfBoundsException ex){
+            
         }
-        catch (RuntimeException ex){
-            System.out.println("RuntimeException");
+        finally {
+            System.out.println("Statement 4"); // do every if have or not exception
         }
-        catch (Exception ex){
-            System.out.println("Exception");
-        }
+        System.out.println("Statement 5");
     }
 }
