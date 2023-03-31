1e1135ec08bced3a8128c6fb6fa2f25e46055935
FameNu <phuwamet.fame@gmail.com>
2023-03-31 21:17:22 +0700

    Send Assign

--- src/main/java/project2Class2/MyProjectInClass.java
+++ src/main/java/project2Class2/MyProjectInClass.java
@@ -2,19 +2,19 @@ package project2Class2;
 
 public class MyProjectInClass {
     public static void main(String[] args) {
-//        try {
-//            int[] list = new int[10];
-//            System.out.println("list[10] is " + list[10]);
-//        }
-//        catch (ArithmeticException ex){
-//            System.out.println("ArithmeticException");
-//        }
-//        catch (RuntimeException ex){
-//            System.out.println("RuntimeException");
-//        }
-//        catch (Exception ex){
-//            System.out.println("Exception");
-//        }
+        try {
+            int[] list = new int[10];
+            System.out.println("list[10] is " + list[10]);
+        }
+        catch (ArithmeticException ex){
+            System.out.println("ArithmeticException");
+        }
+        catch (RuntimeException ex){
+            System.out.println("RuntimeException");
+        }
+        catch (Exception ex){
+            System.out.println("Exception");
+        }
         try {
             System.out.println("Statement 1");
             System.out.println(1/0);
